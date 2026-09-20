ff4dd9360c5b2a9d12e0528ede79c4b9ba98d325
Ryan <ryanteo235@gmail.com>
2026-09-20 15:22:05 +0800

    init work added sprites, moving and single jump

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10;
+    private Rigidbody2D marioBody;
+
+    public float upSpeed = 5;
+    private bool onGroundState = true;
+    private SpriteRenderer marioSprite;
+    private bool faceRightState = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set to be 30 FPS
+        Application.targetFrameRate =  30;
+        marioBody = GetComponent<Rigidbody2D>();
+        marioSprite = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown("a") && faceRightState){
+          faceRightState = false;
+          marioSprite.flipX = true;
+      }
+
+      if (Input.GetKeyDown("d") && !faceRightState){
+          faceRightState = true;
+          marioSprite.flipX = false;
+      }
+    }
+    public float maxSpeed = 20;
+    // FixedUpdate is called 50 times a second
+    void  FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Mathf.Abs(moveHorizontal) > 0){
+            Vector2 movement = new Vector2(moveHorizontal, 0);
+            // check if it doesn't go beyond maxSpeed
+            if (marioBody.linearVelocity.x < maxSpeed)
+                    marioBody.AddForce(movement * speed);
+        }
+
+        // stop
+        if (Input.GetKeyUp("a") || Input.GetKeyUp("d")){
+            // stop
+            marioBody.linearVelocity = new Vector2(0, marioBody.linearVelocity.y);
+        }
+
+        if (Input.GetKeyDown("space") && onGroundState){
+            marioBody.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
+            onGroundState = false;
+        }
+  } 
+
+  void OnCollisionEnter2D(Collision2D col)
+  {
+      if (col.gameObject.CompareTag("Ground")) onGroundState = true;
+  }
+
+  
+
+}
