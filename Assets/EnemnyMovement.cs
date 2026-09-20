a0f99cabca9fbeb561d6b4dfc65517fe7074fcd3
Ryan <ryanteo235@gmail.com>
2026-09-20 17:08:41 +0800

    implemented enemy movement

---
+++ Assets/EnemnyMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    private float originalX;
+    private float maxOffset = 5.0f;
+    private float enemyPatroltime = 2.0f;
+    private int moveRight = -1;
+    private Vector2 velocity;
+
+    private Rigidbody2D enemyBody;
+    private SpriteRenderer goombaSprite;
+
+    void Start()
+    {
+        enemyBody = GetComponent<Rigidbody2D>();
+        goombaSprite = GetComponent<SpriteRenderer>();
+        // get the starting position
+        originalX = transform.position.x;
+        ComputeVelocity();
+    }
+    void ComputeVelocity()
+    {
+        velocity = new Vector2((moveRight) * maxOffset / enemyPatroltime, 0);
+    }
+    void Movegoomba()
+    {
+        enemyBody.MovePosition(enemyBody.position + velocity * Time.fixedDeltaTime);
+    }
+
+    void FixedUpdate()
+    {   
+        if (moveRight > 0)
+        {
+            goombaSprite.flipX = false;
+        } else
+        {
+            goombaSprite.flipX = true;
+        }
+
+        if (Mathf.Abs(enemyBody.position.x - originalX) < maxOffset)
+        {// move goomba
+            Movegoomba();
+        }
+        else
+        {
+            // change direction
+            moveRight *= -1;
+            ComputeVelocity();
+            Movegoomba();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // change direction on collision
+        moveRight *= -1;
+        ComputeVelocity();
+        Movegoomba();
+
+    }
+}
