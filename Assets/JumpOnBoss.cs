af5d997a05c00fee75fefb97a54228864c0e6a46
Ryan <ryanteo235@gmail.com>
2026-09-24 00:01:48 +0800

    jumponboss using same ground check state

--- Assets/JumpOnBoss.cs
+++ Assets/JumpOnBoss.cs
@@ -7,14 +7,10 @@ public class JumpOnBoss : MonoBehaviour
 {
     public Transform enemyLocation;
     // public TextMeshProUGUI scoreText;
-    private bool onGroundState;
 
     // [System.NonSerialized]
     // public int score = 0; // we don't want this to show up in the inspector
 
-    public Vector3 boxSize;
-    public float maxDistance;
-    public LayerMask layerMask;
     public float bounceSpeed = 5.0f;   // lil bump after stomp
     private Rigidbody2D marioBody;
     private PlayerMovement playerMovement;
@@ -26,24 +22,11 @@ public class JumpOnBoss : MonoBehaviour
         playerMovement = GetComponent<PlayerMovement>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        // mario jumps -- input must be polled here, not in FixedUpdate
-        if (Input.GetKeyDown("space") && onGroundCheck())
-        {
-            onGroundState = false;
-        }
-    }
-
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.CompareTag("Ground")) onGroundState = true;
-
-        // mario stomps the Boss: airborne from a jump, haven't scored yet this
-        // jump, and mario is above the goomba (so side hits don't count)
-        // Mario collides with Boss
-        if (col.gameObject.CompareTag("Boss"))
+        // mario stomps the Boss: airborne, and landing clearly on top of it.
+        // usinng ground state from playermovement
+        if (col.gameObject.CompareTag("Boss") && !playerMovement.isOnGround)
         {
             ContactPoint2D contact = col.GetContact(0);
 
@@ -56,25 +39,5 @@ public class JumpOnBoss : MonoBehaviour
                 marioBody.AddForce(Vector2.up * bounceSpeed, ForceMode2D.Impulse);
             }
         }
-    }
-
-
-    private bool onGroundCheck()
-    {
-        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, maxDistance, layerMask))
-        {
-            Debug.Log("on ground");
-            return true;
-        }
-        else
-        {
-            Debug.Log("not on ground");
-            return false;
-        }
-    }
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawCube(transform.position - transform.up * maxDistance, boxSize);
     }
 }
